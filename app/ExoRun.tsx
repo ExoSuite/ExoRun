192e6efa90f6b62fae78041699dfc2e5aee3d3ca
Loïc Lopez <loic.lopez@epitech.eu>
2018-09-26 11:04:00 -0400

    add base project

---
+++ app/ExoRun.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {Container} from "./components";
+
+export class ExoRun extends React.Component {
+
+    render() {
+        return (
+            <Container>
+                <Text>Hello</Text>
+            </Container>
+        );
+    }
+}
+
+const styles = StyleSheet.create({});
