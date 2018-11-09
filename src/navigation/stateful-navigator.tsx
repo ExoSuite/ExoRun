00baaea6541afeff068bf97f7b307f0ac34d8c89
Loïc Lopez <loic.lopez@epitech.eu>
2018-11-09 01:14:10 -0500

    remove loader

--- src/navigation/stateful-navigator.tsx
+++ src/navigation/stateful-navigator.tsx
@@ -1,7 +1,11 @@
 import * as React from "react";
 import { inject, observer } from "mobx-react";
+import { action, observable } from "mobx";
+import { StyleSheet } from 'react-native';
 // @ts-ignore: until they update @type/react-navigation
 import { getNavigation, NavigationScreenProp, NavigationState } from "react-navigation";
+import Loader from "react-native-mask-loader";
+
 import { RootNavigator } from "./root-navigator";
 import { NavigationStore } from "./navigation-store";
 
@@ -14,13 +18,26 @@ interface StatefulNavigatorProps {
 export class StatefulNavigator extends React.Component<StatefulNavigatorProps, {}> {
   currentNavProp: NavigationScreenProp<NavigationState>;
 
+  @observable appLoaded: boolean = false;
+
   getCurrentNavigation = () => {
     return this.currentNavProp;
   };
 
+  @action.bound
+  removeLoader() {
+    this.appLoaded = true;
+  }
+
+  async componentDidMount() {
+      setTimeout(this.removeLoader, 2000);
+  }
+
   render() {
     // grab our state & dispatch from our navigation store
     const { state, dispatch, actionSubscribers } = this.props.navigationStore;
+
+    const { appLoaded } = this;
 
     // create a custom navigation implementation
     this.currentNavProp = getNavigation(
@@ -29,9 +46,33 @@ export class StatefulNavigator extends React.Component<StatefulNavigatorProps, {
       dispatch,
       actionSubscribers(),
       {},
-      this.getCurrentNavigation,
+      this.getCurrentNavigation
     );
 
-    return <RootNavigator navigation={this.currentNavProp} />;
+    return (
+      <Loader
+        isLoaded={appLoaded}
+        imageSource={require("./twitter.png")}
+        backgroundStyle={styles.loadingBackgroundStyle}
+      >
+        <RootNavigator navigation={this.currentNavProp}/>
+      </Loader>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  loadingBackgroundStyle: {
+    backgroundColor: 'rgba(125, 125, 255, 1)',
+  },
+});
