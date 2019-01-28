e2ef66462b026f9ed27e2bb7b60b44bb13c0c956
Loïc Lopez <loic.lopez@epitech.eu>
2019-01-28 12:49:48 -0500

    new architecture

--- test/storage.test.ts
+++ test/storage.test.ts
@@ -1,52 +1,52 @@
-import { load, loadString, save, saveString, clear, remove } from "src/lib/storage/storage";
+import { load, loadString, save, saveString, clear, remove } from "../app/utils/storage"
 
 // fixtures
-const VALUE_OBJECT = { x: 1 };
-const VALUE_STRING = JSON.stringify(VALUE_OBJECT);
+const VALUE_OBJECT = { x: 1 }
+const VALUE_STRING = JSON.stringify(VALUE_OBJECT)
 
 // mocks
-const mockGetItem = jest.fn().mockReturnValue(Promise.resolve(VALUE_STRING));
-const mockSetItem = jest.fn();
-const mockRemoveItem = jest.fn();
-const mockClear = jest.fn();
+const mockGetItem = jest.fn().mockReturnValue(Promise.resolve(VALUE_STRING))
+const mockSetItem = jest.fn()
+const mockRemoveItem = jest.fn()
+const mockClear = jest.fn()
 
 // replace AsyncStorage
 jest.mock("AsyncStorage", () => ({
   getItem: mockGetItem,
   setItem: mockSetItem,
   removeItem: mockRemoveItem,
-  clear: mockClear
-}));
+  clear: mockClear,
+}))
 
 // reset mocks after each test
-afterEach(() => jest.clearAllMocks());
+afterEach(() => jest.clearAllMocks())
 
 test("load", async () => {
-  const value = await load("something");
-  expect(value).toEqual(JSON.parse(VALUE_STRING));
-});
+  const value = await load("something")
+  expect(value).toEqual(JSON.parse(VALUE_STRING))
+})
 
 test("loadString", async () => {
-  const value = await loadString("something");
-  expect(value).toEqual(VALUE_STRING);
-});
+  const value = await loadString("something")
+  expect(value).toEqual(VALUE_STRING)
+})
 
 test("save", async () => {
-  await save("something", VALUE_OBJECT);
-  expect(mockSetItem).toHaveBeenCalledWith("something", VALUE_STRING);
-});
+  await save("something", VALUE_OBJECT)
+  expect(mockSetItem).toHaveBeenCalledWith("something", VALUE_STRING)
+})
 
 test("saveString", async () => {
-  await saveString("something", VALUE_STRING);
-  expect(mockSetItem).toHaveBeenCalledWith("something", VALUE_STRING);
-});
+  await saveString("something", VALUE_STRING)
+  expect(mockSetItem).toHaveBeenCalledWith("something", VALUE_STRING)
+})
 
 test("remove", async () => {
-  await remove("something");
-  expect(mockRemoveItem).toHaveBeenCalledWith("something");
-});
+  await remove("something")
+  expect(mockRemoveItem).toHaveBeenCalledWith("something")
+})
 
 test("clear", async () => {
-  await clear();
-  expect(mockClear).toHaveBeenCalledWith();
-});
+  await clear()
+  expect(mockClear).toHaveBeenCalledWith()
+})
