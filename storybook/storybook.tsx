d41a0ad6abc876f606fe1b611fb7e97bb4d96823
Loïc Lopez <loic.lopez@epitech.eu>
2019-02-04 23:11:03 -0500

    fix storybook

--- storybook/storybook.tsx
+++ storybook/storybook.tsx
@@ -2,12 +2,19 @@ import React from "react"
 import { getStorybookUI, configure } from "@storybook/react-native"
 import SplashScreen from "react-native-splash-screen"
 import { Provider } from "mobx-react/native"
+import Config from "react-native-config"
 
 configure(() => {
   require("./storybook-registry")
 })
 
-const StorybookUI = getStorybookUI({ port: 9001, host: "localhost", onDeviceUI: true })
+let StorybookUI;
+if (Config.SERVER_IP) {
+  StorybookUI = getStorybookUI({ port: 9001, host: Config.SERVER_IP, onDeviceUI: true })
+}
+else {
+  StorybookUI = getStorybookUI({ port: 9001, host: "localhost", onDeviceUI: true })
+}
 
 // RN hot module must be in a class for HMR
 export class StorybookUIRoot extends React.Component {
