f5b1dc28176b5935988dd928ba63fb40f683d03e
Loïc Lopez <loic.lopez@epitech.eu>
2019-02-14 22:16:54 -0500

    fix android build

--- android/app/src/main/java/com/exorun/MainActivity.java
+++ android/app/src/main/java/com/exorun/MainActivity.java
@@ -3,6 +3,10 @@ package com.exorun;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
 
 public class MainActivity extends ReactActivity {
 
