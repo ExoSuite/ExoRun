c6898527a71917372f90eab12b43fdce22ebea1d
Loïc Lopez <loic.lopez@epitech.eu>
2019-05-04 18:18:57 -0400

    try improving code coverage

---
+++ test/tests/app/enzyme/login.screen.enzyme.test.tsx
@@ -0,0 +1,35 @@
+/**
+ * @jest-environment jsdom
+ */
+import * as React from "react"
+import { mount } from "enzyme"
+import { LoginScreen } from "@screens/auth"
+import { Image, SafeAreaView } from "react-native"
+import { Screen } from "@components/screen"
+import { AnimatedInteractiveInput } from "@components/animated-interactive-input"
+import { FormRow } from "@components/form-row"
+import { KeyboardAccessoryView } from "react-native-keyboard-accessory"
+import { Provider } from "mobx-react/native"
+import { Api } from "@services/api"
+import { SoundPlayer } from "@services/sound-player"
+
+describe("login tests", () => {
+  const api: Api = new Api()
+  const soundPlayer = new SoundPlayer()
+
+  test("login render correctly", () => {
+    const wrapper = mount<LoginScreen>((
+      <Provider api={api} soundPlayer={soundPlayer}>
+        <LoginScreen navigation={{goBack: (): boolean => true}}/>
+      </Provider>
+    ))
+    wrapper.render()
+    expect(wrapper.find(SafeAreaView)).toExist()
+    expect(wrapper.find(Screen)).toExist()
+    expect(wrapper.find(AnimatedInteractiveInput)).toExist()
+    expect(wrapper.find(FormRow)).toExist()
+    expect(wrapper.find(Image)).toExist()
+    expect(wrapper.find(KeyboardAccessoryView)).toExist()
+  })
+
+})
