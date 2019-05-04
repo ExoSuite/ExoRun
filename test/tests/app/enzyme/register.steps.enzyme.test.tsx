dbf4b4874b51183f71dfa3dfd1992532643d1437
Loïc Lopez <loic.lopez@epitech.eu>
2019-05-04 18:07:28 -0400

    try improving code coverage

--- test/tests/app/enzyme/register.steps.enzyme.test.tsx
+++ test/tests/app/enzyme/register.steps.enzyme.test.tsx
@@ -3,17 +3,31 @@
  */
 import * as React from "react"
 import { mount } from "enzyme"
-import { SecondStepRegisterScreenImpl } from "@screens/auth"
+import { FirstStepRegisterScreen, SecondStepRegisterScreenImpl } from "@screens/auth"
 import { Image, SafeAreaView } from "react-native"
 import { Screen } from "@components/screen"
 import { AnimatedInteractiveInput } from "@components/animated-interactive-input"
 import { FormRow } from "@components/form-row"
 import { KeyboardAccessoryView } from "react-native-keyboard-accessory"
 
-describe("second step register screen loader tests", () => {
-  test("render correctly", () => {
+describe("register steps", () => {
+  test("second step register render correctly", () => {
     // @ts-ignore
     const wrapper = mount<SecondStepRegisterScreenImpl>(<SecondStepRegisterScreenImpl navigation={{}}/>)
+    wrapper.render()
+
+    expect(wrapper.find(SafeAreaView)).toExist()
+    expect(wrapper.find(Screen)).toExist()
+    expect(wrapper.find(AnimatedInteractiveInput)).toExist()
+    expect(wrapper.find(FormRow)).toExist()
+    expect(wrapper.find(Image)).toExist()
+    expect(wrapper.find(KeyboardAccessoryView)).toExist()
+  })
+
+  test("first step register render correctly", () => {
+    // @ts-ignore
+    const wrapper = mount<FirstStepRegisterScreen>(<FirstStepRegisterScreen navigation={{}}/>)
+    wrapper.render()
 
     expect(wrapper.find(SafeAreaView)).toExist()
     expect(wrapper.find(Screen)).toExist()
