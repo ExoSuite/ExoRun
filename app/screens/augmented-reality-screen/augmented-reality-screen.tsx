11330a3c53f46952a276e78e093ea1824afc41aa
renaud juliani <renaud.juliani@exosuite.fr>
2019-07-25 16:04:47 +0200

    Push à récup pour merge avec eric

--- app/screens/augmented-reality-screen/augmented-reality-screen.tsx
+++ app/screens/augmented-reality-screen/augmented-reality-screen.tsx
@@ -2,13 +2,17 @@ import { color } from "@theme"
 import { action, observable } from "mobx"
 import { observer } from "mobx-react"
 import * as React from "react"
-import { ViewStyle } from "react-native"
+import { View, ViewStyle } from "react-native"
 import { NavigationScreenProps } from "react-navigation"
-import { ViroARScene, ViroConstants, ViroText } from "react-viro"
+import { ViroARScene, ViroConstants, ViroText, ViroAmbientLight, Viro3DObject, ViroARCamera, ViroBox, ViroButton } from "react-viro"
+import { TextInput } from "react-native-paper"
 
 export interface IAugmentedRealityScreenProps extends NavigationScreenProps<{}> {
 }
 
+const Animation = require("../../assets/3Dasset/IdleAndRun.vrx");
+const AnimationRun = require("../../assets/3Dasset/IdleAndRunFastAndSlow.vrx");
+const Arrow = require("../../assets/3Dasset/Arrow.vrx");
 const ROOT: ViewStyle = {
   backgroundColor: color.background
 }
@@ -19,12 +23,44 @@ const ROOT: ViewStyle = {
 @observer
 export class AugmentedRealityScreen extends React.Component<IAugmentedRealityScreenProps> {
 
+  @observable private AnimationName = "Idle"
+  @observable private Speed = "RunFast";
   @observable private text = "Initializing AR..."
+
+  @action.bound
+  private AdaptSpeed(scrollPosition: any, source: any): void {
+    console.log(scrollPosition[0] + scrollPosition[1]);
+  }
 
   @action.bound
   private onInitialized(state: string, reason: string): void {
     if (state === ViroConstants.TRACKING_NORMAL) {
+      this.AnimationName = "Idle"
       this.text = "Hello World!"
+    }
+  }
+
+  @action.bound
+  private SwitchAnimation(): void {
+    if (this.AnimationName === "Idle") {
+      this.AnimationName = this.Speed;
+    }  else {
+      this.AnimationName = "Idle";
+    }
+  }
+  @action.bound
+  private switchSpeed() : void {
+    if (this.Speed === "RunFast") {
+      this.Speed = "RunSlow";
+      if (this.AnimationName !== "Idle") {
+        this.AnimationName = this.Speed;
+      }
+    }
+    else if (this.Speed === "RunSlow") {
+      this.Speed = "RunFast";
+      if (this.AnimationName !== "Idle") {
+        this.AnimationName = this.Speed;
+      }
     }
   }
 
@@ -33,12 +69,47 @@ export class AugmentedRealityScreen extends React.Component<IAugmentedRealityScr
 
     return (
       <ViroARScene style={ROOT} onTrackingUpdated={this.onInitialized}>
-        <ViroText
-          text={text}
-          scale={[0.5, 0.5, 0.5]}
-          position={[0, 0, -1]}
-          style={{ fontSize: 30, color: "#ffffff", textAlignVertical: "center", textAlign: "center" }}
-        />
+        <ViroAmbientLight color="#ffffff" />
+        <ViroARCamera>
+          <Viro3DObject
+            source={Arrow}
+            highAccuracyEvents
+            position={[0, 0.7, -2.1]}
+            scale={[0.1, 0.1, 0.1]}
+            rotation={[0, 0, 0]}
+            type="VRX"
+            transformBehaviors={["billboard"]}
+          />
+          <Viro3DObject
+            source={AnimationRun}
+            highAccuracyEvents
+            position={[0, -0.8, -3]}
+            scale={[0.009, 0.009, 0.009]}
+            rotation={[0, 180, 0]}
+            type="VRX"
+            onClick={this.SwitchAnimation}
+            animation={{name: this.AnimationName,
+                        run: true,
+                        loop: true,
+                        interruptible: true}}
+          />
+
+          <ViroButton
+            source={require("../../assets/3Dasset/BBackground.png")}
+            position={[-2, -4, -10]}
+            height={1}
+            width={1.5}
+            onClick={this.SwitchAnimation}
+          />
+          <ViroButton
+            source={require("../../assets/3Dasset/BBackgroundSpeed.png")}
+            position={[2, -4, -10]}
+            height={1}
+            width={1.5}
+            onClick={this.switchSpeed}
+          />
+
+        </ViroARCamera>
       </ViroARScene>
     )
   }
