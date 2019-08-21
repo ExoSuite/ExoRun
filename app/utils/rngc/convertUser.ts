0e7120b824f24ecc0dbc1902ae6a354da01bf104
loiclopez <loic.lopez@exosuite.fr>
2019-08-21 18:44:22 +0200

    working chat, just need the filter on the groups

--- app/utils/rngc/convertUser.ts
+++ app/utils/rngc/convertUser.ts
@@ -1,6 +1,7 @@
 import { IUser } from "@services/api"
+import { User as RNGCUser } from "react-native-gifted-chat"
 
-export function convertUserToRNGCFormat(user: IUser, avatarUrl: string) {
+export function convertUserToRNGCFormat(user: IUser, avatarUrl: string): RNGCUser {
   return {
     _id: user.id,
     name: `${user.first_name} ${user.last_name}`,
