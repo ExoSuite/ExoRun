d5ee6530bcc398f2b58312dbbfb3d6809f6b138f
loiclopez <loic.lopez@exosuite.fr>
2019-10-31 18:15:37 +0100

    format code

--- hacks/hasteImpl.js
+++ hacks/hasteImpl.js
@@ -8,70 +8,66 @@
  * @flow
  */
 
-'use strict';
+"use strict"
 
-const path = require('path');
-const cli = require('@react-native-community/cli');
+const path = require("path")
+const cli = require("@react-native-community/cli")
 
 // Use duck-typing because of Facebook-internal infra that doesn't have the cli package.
-const {haste} = (cli.loadConfig && cli.loadConfig()) || {
+const { haste } = (cli.loadConfig && cli.loadConfig()) || {
   haste: {
     providesModuleNodeModules: [],
-    platforms: ['ios', 'android'],
+    platforms: ["ios", "android"],
   },
-};
+}
 
 // Detect out-of-tree platforms and add them to the whitelists
 const pluginRoots /*: Array<string> */ = haste.providesModuleNodeModules.map(
-  name => path.resolve(__dirname, '../../', name) + path.sep,
-);
+  name => path.resolve(__dirname, "../../", name) + path.sep,
+)
 
-const pluginNameReducers /*: Array<[RegExp, string]> */ = haste.platforms.map(
-  name => [new RegExp(`^(.*)\\.(${name})$`), '$1'],
-);
+const pluginNameReducers /*: Array<[RegExp, string]> */ = haste.platforms.map(name => [
+  new RegExp(`^(.*)\\.(${name})$`),
+  "$1",
+])
 
-const ROOTS = [path.resolve(__dirname, '..') + path.sep, ...pluginRoots];
+const ROOTS = [path.resolve(__dirname, "..") + path.sep, ...pluginRoots]
 
 const BLACKLISTED_PATTERNS /*: Array<RegExp> */ = [
   /.*[\\\/]__(mocks|tests)__[\\\/].*/,
   /^Libraries[\\\/]Animated[\\\/]src[\\\/]polyfills[\\\/].*/,
   /^Libraries[\\\/]Renderer[\\\/]fb[\\\/].*/,
   /DerivedData[\\\/].*/,
-];
+]
 
-const WHITELISTED_PREFIXES /*: Array<string> */ = [
-  'IntegrationTests',
-  'Libraries',
-  'ReactAndroid',
-  'RNTester',
-];
+const WHITELISTED_PREFIXES /*: Array<string> */ = ["IntegrationTests", "Libraries", "ReactAndroid", "RNTester"]
 
 const NAME_REDUCERS /*: Array<[RegExp, string]> */ = [
   // extract basename
-  [/^(?:.*[\\\/])?([a-zA-Z0-9$_.-]+)$/, '$1'],
+  [/^(?:.*[\\\/])?([a-zA-Z0-9$_.-]+)$/, "$1"],
   // strip .js/.js.flow suffix
-  [/^(.*)\.js(\.flow)?$/, '$1'],
+  [/^(.*)\.js(\.flow)?$/, "$1"],
   // strip native suffix
-  [/^(.*)\.(native)$/, '$1'],
+  [/^(.*)\.(native)$/, "$1"],
   // strip plugin platform suffixes
   ...pluginNameReducers,
-];
+]
 
 function isHastePath(filePath /*: string */) /*: boolean */ {
-  if (!filePath.endsWith('.js') && !filePath.endsWith('.js.flow')) {
-    return false;
+  if (!filePath.endsWith(".js") && !filePath.endsWith(".js.flow")) {
+    return false
   }
 
-  const root = ROOTS.find(r => filePath.startsWith(r));
+  const root = ROOTS.find(r => filePath.startsWith(r))
   if (!root) {
-    return false;
+    return false
   }
 
-  filePath = filePath.substr(root.length);
+  filePath = filePath.substr(root.length)
   if (BLACKLISTED_PATTERNS.some(pattern => pattern.test(filePath))) {
-    return false;
+    return false
   }
-  return WHITELISTED_PREFIXES.some(prefix => filePath.startsWith(prefix));
+  return WHITELISTED_PREFIXES.some(prefix => filePath.startsWith(prefix))
 }
 
 module.exports = {
@@ -79,19 +75,16 @@ module.exports = {
    * @return {string|void} hasteName for module at filePath; or undefined if
    *                       filePath is not a haste module
    */
-  getHasteName(
-    filePath /*: string */,
-    sourceCode /*: ?string */,
-  ) /*: string | void */ {
+  getHasteName(filePath /*: string */, sourceCode /*: ?string */) /*: string | void */ {
     if (!isHastePath(filePath)) {
-      return undefined;
+      return undefined
     }
 
     const hasteName = NAME_REDUCERS.reduce(
       (name, [pattern, replacement]) => name.replace(pattern, replacement),
       filePath,
-    );
+    )
 
-    return hasteName;
+    return hasteName
   },
-};
+}
