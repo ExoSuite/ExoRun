649f37d7753f3667b7bc9cc8fe99d2136fb3e8a0
loiclopez <loic.lopez@exosuite.fr>
2019-11-16 19:03:38 +0100

    fix multiples issues

--- app/navigation/root-navigator.ts
+++ app/navigation/root-navigator.ts
@@ -1,11 +1,9 @@
 import { AppNavigator } from "@navigation/app-navigator"
 import { createAppContainer, createSwitchNavigator } from "react-navigation"
 import { AuthStack } from "./auth-navigator"
-import { PendingRequestsScreen } from "@screens/pending-requests-screen"
 
 export const RootNavigator = createAppContainer(createSwitchNavigator(
   {
-    pendingRequestsScreen: { screen: PendingRequestsScreen },
     Auth: AuthStack,
     App: AppNavigator
   },
